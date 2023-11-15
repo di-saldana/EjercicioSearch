704b8ffd712863211f846fcadedcb39cd833e7fd
di-saldana <dsl42@alu.ua.es>
2023-11-15 19:38:44 +0100

    Initial Commit

---
+++ ejercicio_search/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ejercicio_search
+//
+//  Created by Dianelys Saldaña on 11/15/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
